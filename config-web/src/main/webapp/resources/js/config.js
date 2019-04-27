0d7bc9cc30f343da4d2420bf3ae1db2dabbe564c
CRUD20152242 <2314449060@qq.com>
2019-04-28 01:12:45 +0800

    新增前端工作量

--- config-web/src/main/webapp/resources/js/config.js
+++ config-web/src/main/webapp/resources/js/config.js
@@ -1,4 +1,41 @@
-$(function () {
-    //启动弹出框
-    $('[data-toggle="popover"]').popover(options)
-})
+$(
+    function () {
+        
+    }
+)
+var count = 2;
+$("#addIteam").click(addkey)
+function addkey() {
+    count++;
+   var res = "<div class='input-group' id='keyGroup"+count+"'>" +
+       "                        <span class='input-group-addon' >key = </span>" +
+       "                        <input type='text' class='form-control' id='key"+count+"' aria-label='...'>" +
+       "                    </div>"+
+       "    <br class='keyGroup"+count+"'/>"
+
+    $("#addKey").append(res)
+
+    var addValue = "    <div class='input-group' id='valueGroup"+count+"'>" +
+        "                        <span class='input-group-addon value1'>value = </span>" +
+        "                        <input type='text' class='form-control' class='value"+count+"' aria-label='...'>" +
+        "                    </div>" +
+        "    <br class='valueGroup"+count+"'/>"
+
+    $("#addValue").append(addValue)
+}
+$("#deleteIteam").click(deleteIteam)
+
+function deleteIteam() {
+    $(".keyGroup"+count).remove()
+    $(".valueGroup"+count).remove()
+    $("#keyGroup"+count).remove()
+    $("#valueGroup"+count).remove()
+
+    count--
+
+    if(count<3){
+        count=2;
+    }
+
+
+}
